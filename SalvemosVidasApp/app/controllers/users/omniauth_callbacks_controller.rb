b5eb14aea416cc66e0d3d480d719461ee40945da
lelthar <geraldm1998@hotmail.com>
2019-01-04 18:42:12 -0600

    Se agregó un mensaje de error para el login con facebook

--- SalvemosVidasApp/app/controllers/users/omniauth_callbacks_controller.rb
+++ SalvemosVidasApp/app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Users::OmniauthCallbacksController < ApplicationController
 		if @user.persisted? #Revisa si se pudo guardar exitosamente
 			@user.remember_me = true #Recuerda el login si el navegador es cerrado
 			sign_in_and_redirect @user, event: :authentication
-		
+			return
 		else
 
 			session["devise.auth"] = request.env["omniauth.auth"]
@@ -14,7 +14,11 @@ class Users::OmniauthCallbacksController < ApplicationController
 			render :edit, user: @user
 		end
 		
-	end 
+	end
+
+	def failure
+		redirect_to new_user_session_path, notice: "Error: #{params[:error_message]}. Motivo: #{params[:error_description]}"
+	end
 
 	def custom_sign_up
 		@user = User.from_omniauth(session["devise.auth"])
